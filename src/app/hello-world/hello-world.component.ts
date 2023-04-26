ff9a7984b8b5f602ed27b08848dea3237f951c1c
basimkarhabi <basim.karhabi@gmail.com>
2023-04-26 14:55:57 +0200

    first commit1

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,13 @@
+// Importing Your Dependencies
+import { Component, OnInit } from '@angular/core';
+//declaring the component
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+  costructor() { }
+  ngOnInit() { }
+
+}
